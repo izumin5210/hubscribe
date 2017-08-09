aec593f54a75ab6b206a8b404d4725727ff7e3b4
izumin5210 <masayuki@izumin.info>
2017-08-09 23:22:11 +0900

    rid rails g model github/login_name

---
+++ spec/models/github/login_name_spec.rb
@@ -0,0 +1,16 @@
+# ## Schema Information
+#
+# Table name: `github_login_names`
+#
+# ### Columns
+#
+# Name      | Type               | Attributes
+# --------- | ------------------ | ---------------------------
+# **`id`**  | `string`           | `not null, primary key`
+#
+
+require 'rails_helper'
+
+RSpec.describe Github::LoginName, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
