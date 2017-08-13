b890afb13ac342842a0992872cd381ccb64d3ce1
izumin5210 <masayuki@izumin.info>
2017-08-13 16:41:44 +0900

    Implement auth/session#create

--- app/controllers/auth/session_controller.rb
+++ app/controllers/auth/session_controller.rb
@@ -1,2 +1,13 @@
 class Auth::SessionController < ApplicationController
+  def create
+    user = Auth::User.find_or_create_by_auth_params!(auth_params, user: current_user)
+    sign_in user
+    redirect_to root_path
+  end
+
+  private
+
+  def auth_params
+    request.env['omniauth.auth']
+  end
 end
